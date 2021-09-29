672498d6bca006de7866340af7ee56f07fd2fffe
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-29 13:40:27 +0530

    Hemanth : Add Multiple contacts to Address Book

---
+++ UC5AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC5AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details ob = new Details();
+            ob.AddMultiple();
+        }
+    }
+}
